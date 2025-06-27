ec56e29abd163bd35559e3e061b3e200c928f2d9
Michael Lopes <michael.designer@protonmail.com>
2025-06-27 17:31:05 -0300

    moved images to project-3d and javascript burger and faq is working

--- js/script.js
+++ js/script.js
@@ -1,12 +1,54 @@
-const elements = {
-    faq: document.querySelectorAll('.box-questions'),
+const elements = {    
+    burgerToggle: document.querySelector('.toggle'),
+    menu: document.querySelector('.menu-toggle'),
+    close: document.querySelector('.cross'),
+    itensMenu: document.querySelectorAll('.menu-toggle__itens'),
+    faq: document.querySelectorAll('.box-faq__question'),
+    boxFaq: document.querySelectorAll('.box-faq'),
     resposta: document.querySelectorAll('.response'),
-    chevron: document.querySelectorAll('.fi-br-angle-small-down')    
+    chevron: document.querySelectorAll('.fi-ts-angle-small-down')    
 }
+
+elements.burgerToggle.addEventListener('click', () =>{
+    elements.menu.classList.toggle('menu-toggle--active');
+})
+
+elements.close.addEventListener('click', () =>{
+    elements.burgerToggle.dispatchEvent(new Event('click'));
+})
+
+elements.itensMenu.forEach(item =>{
+    item.addEventListener('click', () =>{
+        elements.burgerToggle.dispatchEvent(new Event('click'));
+        console.log('clicou no item')
+    })
+})
+
+let positionLinks = document.querySelector('.project__photo--over');
+
+let hrefElement = positionLinks.children
+
+Array.from(hrefElement).forEach(item =>{
+    item.addEventListener('click', () =>{
+        let title = item.getAttribute('src');
+        
+        // Substitui o caminho dentro do atributo href do elemento clicado
+        item.setAttribute('src', './sources/project-3d/projeto-academia_07.png');
+        console.log(title);
+    })
+})
+
 
 elements.faq.forEach((item, index) => {
     item.addEventListener('click', () => {
-        elements.chevron[index].classList.toggle('active');
-        elements.resposta[index].classList.toggle('active');
+        elements.boxFaq[index].classList.toggle('box-faq--active');
+        if (elements.boxFaq[index].classList.contains('box-faq--active')){
+            elements.chevron[index].style.rotate = "-90deg";
+        }else{
+            elements.chevron[index].style.rotate = "0deg";
+        }
+        elements.resposta[index].classList.toggle('response--active');
+
+        console.log('clicou certo!')
     });
 });
