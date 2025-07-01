f90b33a24c8a8f06b7eae9996703703616251cb7
Michael Lopes <michael.designer@protonmail.com>
2025-07-01 17:35:18 -0300

    home finalized, equipment need to be finished

--- js/script.js
+++ js/script.js
@@ -6,8 +6,130 @@ const elements = {
     faq: document.querySelectorAll('.box-faq__question'),
     boxFaq: document.querySelectorAll('.box-faq'),
     resposta: document.querySelectorAll('.response'),
-    chevron: document.querySelectorAll('.fi-ts-angle-small-down')    
+    chevron: document.querySelectorAll('.fi-ts-angle-small-down'),
+    nxt: document.querySelector('.nxt'),
+    prv:document.querySelector('.prv'),
+    photo: document.getElementById('project-photo'),
+    modal:document.getElementById('modal'),
+    linhaTx:document.getElementById('l01'),
+    linhaSx:document.getElementById('l02'),
+    linhaMac:document.getElementById('l03'),
+    dsModal:document.querySelectorAll('.close'),
+    dsModalPlanos:document.querySelector('.close-md-planos'),
+    modalPlanos: document.getElementById('md-planos'),
+    openModal: document.getElementById('open-md-planos')
 }
+
+elements.openModal.addEventListener('click', () => {
+    elements.modalPlanos.style.display = 'block';    
+})
+
+elements.dsModalPlanos.addEventListener('click', () => {
+    elements.modalPlanos.style.display = 'none';
+})
+
+let equipTx = elements.linhaTx.childNodes;
+let equipSx = elements.linhaSx.childNodes;
+let equipMac = elements.linhaMac.childNodes;
+
+let modals = Array.from(elements.modal.children);
+console.log(equipMac);
+
+equipTx.forEach((item, index) => {
+   
+    item.addEventListener('click', () => {
+         console.log('apertei no array' + index)
+        // Fecha todos os modais antes de abrir um novo
+        modals.forEach(modalEl => {
+            modalEl.style.display = 'none';
+        });
+        switch(index) {
+            case 1:
+            console.log('Deu certo' + index)   
+            modals[0].style.display = 'flex';
+            break;
+            case 3:
+            console.log('Deu certo' + index)   
+            modals[1].style.display = 'flex';
+            break;
+            case 5:
+            console.log('Deu certo' + index)   
+            modals[2].style.display = 'flex';
+            break;
+            case 7:
+            console.log('Deu certo' + index)   
+            modals[3].style.display = 'flex';
+            break;            
+            default:
+                // Caso padrão, se necessário
+                break;
+        }
+    });
+});
+equipSx.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        console.log('apertei no array' + index)
+        modals.forEach(modalEl => {
+            modalEl.style.display = 'none';
+        });
+        switch(index) {
+            case 1: 
+            console.log('Deu certo' + index)
+            modals[4].style.display = 'flex';
+            break;
+            case 3:
+                console.log('Deu certo' + index)
+                modals[5].style.display = 'flex';
+                break;
+            case 5:
+                console.log('Deu certo' + index)
+                modals[6].style.display = 'flex';
+                break;
+            case 7:
+                console.log('Deu certo' + index)
+                modals[7].style.display = 'flex';
+                break;
+            default:
+                break;
+        }
+    })
+})
+equipMac.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        console.log('apertei no array' + index)
+        modals.forEach(modalEl => {
+            modalEl.style.display = 'none';
+        });
+        switch(index) {
+            case 1: 
+            console.log('Deu certo' + index)
+            modals[8].style.display = 'flex';
+            break;
+            case 3:
+                console.log('Deu certo' + index)
+                modals[9].style.display = 'flex';
+                break;
+            case 5:
+                console.log('Deu certo' + index)
+                modals[10].style.display = 'flex';
+                break;
+            case 7:
+                console.log('Deu certo' + index)
+                modals[11].style.display = 'flex';
+                break;
+            default:
+                break;
+        }
+    })
+})
+elements.dsModal.forEach((item) => {
+    item.addEventListener('click', () => {
+        console.log('apertei pra fechar o modal')
+        modals.forEach(modalEl => {
+            modalEl.style.display = 'none';
+        });
+    });
+});
 
 elements.burgerToggle.addEventListener('click', () =>{
     elements.menu.classList.toggle('menu-toggle--active');
@@ -21,20 +143,6 @@ elements.itensMenu.forEach(item =>{
     item.addEventListener('click', () =>{
         elements.burgerToggle.dispatchEvent(new Event('click'));
         console.log('clicou no item')
-    })
-})
-
-let positionLinks = document.querySelector('.project__photo--over');
-
-let hrefElement = positionLinks.children
-
-Array.from(hrefElement).forEach(item =>{
-    item.addEventListener('click', () =>{
-        let title = item.getAttribute('src');
-        
-        // Substitui o caminho dentro do atributo href do elemento clicado
-        item.setAttribute('src', './sources/project-3d/projeto-academia_07.png');
-        console.log(title);
     })
 })
 
